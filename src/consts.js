151c6659d796682e5ba6f591453ebbcc2ba9782e
Michel Bade <m.bade@shopware.com>
2024-03-18 12:23:23 +0100

    Updated

---
+++ src/consts.js
@@ -0,0 +1,16 @@
+export const Sensors = {
+    TEMPERATURE: "Wcg",
+    BRIGHTNESS: "Pdw",
+    HUMIDITY: "ViW",
+    MOTION: "ML4",
+    RGB_BUTTON: "XBe",
+    DUAL_BUTTON: "23Qx",
+    NFC_SCANNER: "22ND",
+}
+
+export const Actors = {
+    PIEZO: "R7N",
+    EPAPER: "XGL",
+    SEG_DISPLAY: "Tre",
+    LCD_DISPLAY: "24Rh",
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Tinkerforge from 'tinkerforge';
+import { Actors, Sensors } from './consts.js';
 
 const ipcon = new Tinkerforge.IPConnection();
+ipcon.connect('172.20.10.242', 4223, console.error);
 
 [`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `uncaughtException`, `SIGTERM`].forEach((e) => {
     process.on(e, () => {
@@ -9,31 +11,30 @@ const ipcon = new Tinkerforge.IPConnection();
     });
 })
 
-ipcon.connect('172.20.10.242', 4223, console.error);
 
-ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
-    function(connectReason) {
-        // Trigger Enumerate
-        ipcon.enumerate();
-    }
-);
+const ps = new Tinkerforge.BrickletPiezoSpeakerV2(Actors.PIEZO, ipcon);
 
-// Register Enumerate Callback
-ipcon.on(Tinkerforge.IPConnection.CALLBACK_ENUMERATE,
-    // Print incoming enumeration
-    function(uid, connectedUid, position, hardwareVersion, firmwareVersion,
-             deviceIdentifier, enumerationType) {
-        console.log('UID:               '+uid);
-        console.log('Enumeration Type:  '+enumerationType);
-        if(enumerationType === Tinkerforge.IPConnection.ENUMERATION_TYPE_DISCONNECTED) {
-            console.log('');
-            return;
-        }
-        console.log('Connected UID:     '+connectedUid);
-        console.log('Position:          '+position);
-        console.log('Hardware Version:  '+hardwareVersion);
-        console.log('Firmware Version:  '+firmwareVersion);
-        console.log('Device Identifier: '+deviceIdentifier);
-        console.log('');
-    }
-);
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED, () => {
+
+    ps.setAlarm(800, 2000, 10, 1, 1, 1000);
+});
+
+// // Register Enumerate Callback
+// ipcon.on(Tinkerforge.IPConnection.CALLBACK_ENUMERATE,
+//     // Print incoming enumeration
+//     function(uid, connectedUid, position, hardwareVersion, firmwareVersion,
+//              deviceIdentifier, enumerationType) {
+//         console.log('UID:               '+uid);
+//         console.log('Enumeration Type:  '+enumerationType);
+//         if(enumerationType === Tinkerforge.IPConnection.ENUMERATION_TYPE_DISCONNECTED) {
+//             console.log('');
+//             return;
+//         }
+//         console.log('Connected UID:     '+connectedUid);
+//         console.log('Position:          '+position);
+//         console.log('Hardware Version:  '+hardwareVersion);
+//         console.log('Firmware Version:  '+firmwareVersion);
+//         console.log('Device Identifier: '+deviceIdentifier);
+//         console.log('');
+//     }
+// );
