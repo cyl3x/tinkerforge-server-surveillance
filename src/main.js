a57409309a3ee41c7db987864ab23dae6a078983
Michel Bade <m.bade@shopware.com>
2024-03-18 12:07:35 +0100

    Added example

--- src/main.js
+++ src/main.js
@@ -1,7 +1,39 @@
-"use strict";
+import Tinkerforge from 'tinkerforge';
 
-import Tinkerforge from 'tinkerforge'
+const ipcon = new Tinkerforge.IPConnection();
 
+[`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `uncaughtException`, `SIGTERM`].forEach((e) => {
+    process.on(e, () => {
+        console.log("closing connection ...");
+        ipcon.disconnect();
+    });
+})
 
-const con = new Tinkerforge.IPConnection();
-con.connect('localhost', 4223, console.error);
+ipcon.connect('172.20.10.242', 4223, console.error);
+
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
+    function(connectReason) {
+        // Trigger Enumerate
+        ipcon.enumerate();
+    }
+);
+
+// Register Enumerate Callback
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_ENUMERATE,
+    // Print incoming enumeration
+    function(uid, connectedUid, position, hardwareVersion, firmwareVersion,
+             deviceIdentifier, enumerationType) {
+        console.log('UID:               '+uid);
+        console.log('Enumeration Type:  '+enumerationType);
+        if(enumerationType === Tinkerforge.IPConnection.ENUMERATION_TYPE_DISCONNECTED) {
+            console.log('');
+            return;
+        }
+        console.log('Connected UID:     '+connectedUid);
+        console.log('Position:          '+position);
+        console.log('Hardware Version:  '+hardwareVersion);
+        console.log('Firmware Version:  '+firmwareVersion);
+        console.log('Device Identifier: '+deviceIdentifier);
+        console.log('');
+    }
+);
