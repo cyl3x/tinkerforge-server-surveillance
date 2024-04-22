e85b895d330fe09bb2035ff43d4070d8fd24ef69
THNMichi <michael.mueller221@gmail.com>
2024-04-22 14:05:54 +0200

    fixed temperature display

--- src/events/display_seg.js
+++ src/events/display_seg.js
@@ -9,6 +9,8 @@ emitter.on(
             .split("")
             .map(Number);
 
-        actors.seg_display.setNumericValue([...digits, 34, 12]);
+        actors.seg_display.setNumericValue([digits[0], digits[1], digits[2], 12]);
+        actors.seg_display.setSelectedSegment(15,true);
+        actors.seg_display.setSelectedSegment(34,true);
     }
 )
