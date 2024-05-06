00264090d56a5f672de8138fcee0ff1bed17f28e
THNMichi <michael.mueller221@gmail.com>
2024-05-06 10:45:55 +0200

    switched back on

--- src/events/index.js
+++ src/events/index.js
@@ -1,6 +1,6 @@
 import "./alarm.js";
 import "./display_seg.js";
-// import "./temperature.js";
+import "./temperature.js";
 import "./humidity.js";
 import "./rgb_button.js";
 import "./lcd_display.js";
